a0c0d961862545f55ea01d5754e39f3565e5fc53
bruce-mig <h180009t@hit.ac.zw>
2023-11-30 09:18:52 +0200

    autoload env variables

--- server/server.go
+++ server/server.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"math/big"
 	"net/http"
+	"os"
 	"strconv"
 	"sync"
 
@@ -18,15 +19,17 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/sirupsen/logrus"
 
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
 )
 
 const (
-	MarketOrder        OrderType = "MARKET"
-	LimitOrder         OrderType = "LIMIT"
-	exchangePrivateKey           = "4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"
-	MarketINN          Market    = "INN"
+	MarketOrder OrderType = "MARKET"
+	LimitOrder  OrderType = "LIMIT"
+	MarketINN   Market    = "INN"
 )
+
+var exchangePrivateKey = os.Getenv("EXCHANGE_PK")
 
 type (
 	OrderType string
@@ -110,9 +113,9 @@ func StartServer() {
 		log.Fatal(err)
 	}
 
-	ex.registerUser("829e924fdf021ba3dbbc4225edfece9aca04b929d6e75613329ca6f1d31c0bb4", 8)
-	ex.registerUser("a453611d9419d0e56f499079478fd72c37b251a94bfde4d19872c44cf65386e3", 7)
-	ex.registerUser("e485d098507f54e7733a205420dfddbe58db035fa577fc294ebd14db90767a52", 6)
+	ex.registerUser(os.Getenv("USER_8_PK"), 8)
+	ex.registerUser(os.Getenv("USER_7_PK"), 7)
+	ex.registerUser(os.Getenv("USER_6_PK"), 6)
 
 	e.GET("/trades/:market", ex.handleGetTrades)
 	e.GET("/order/user/:userID", ex.handleGetOrders)
